af953d1a4e2c336f4956ee8736304acd0590c5a0
GGE95 <122352692+GGE95@users.noreply.github.com>
2023-01-24 11:04:34 +0100

    Update 03_basics.rb

--- lib/03_basics.rb
+++ lib/03_basics.rb
@@ -1,5 +1,5 @@
 def who_is_bigger(a,b,c)
-    if a || b || c = nil
+    if a || b || c == nil
         return "nil detected"
     else
     (a > b && c) ? (return "a is bigger") : (b > a && c ? (return "b is bigger") : (return "c is bigger"))
