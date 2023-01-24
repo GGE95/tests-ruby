cceb37a0235240be649b2c8a22ea003ca2b3c125
Maxime SILVA CRUZ <max.scruz.msc@gmail.com>
2023-01-24 11:01:35 +0100

    ex03

---
+++ lib/03_basics.rb
@@ -0,0 +1,7 @@
+def who_is_bigger(a,b,c)
+    if a || b || c = nil
+        return "nil detected"
+    else
+    (a > b && c) ? (return "a is bigger") : (b > a && c ? (return "b is bigger") : (return "c is bigger"))
+    end
+end
